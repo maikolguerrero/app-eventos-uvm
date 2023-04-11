c18498540a3456434a0044c42e509f3e99fbac61
Fran-JF <franciscouzcategui2021@gmail.com>
2023-04-11 17:19:36 -0400

    Conexion a la Base de Datos

---
+++ app/empresa/query_empresa.js
@@ -0,0 +1,23 @@
+// Importamos el until, la conexion con nuestra base de datos
+const util = require('util')
+const connection = require('../../config/connection')
+// Creamos una variable para realizar las consultas
+const query = util.promisify(connection.query).bind(connection)
+
+// Función para los errores de las peticiones a la base de datos
+async function Empresa(sql) {
+    try {
+        let sql_query = sql
+        const res = await query(sql_query)
+        return res
+    }catch(err) {
+        console.log(`Hubo un error : ${err}`)
+        return err
+    }
+}
+
+// Exportamos
+module.exports = {
+    Empresa,
+    connection
+}
