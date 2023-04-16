ed5c197b90be066806769cde40bc7425d45033ad
Maikol Guerrero <117484381+maikolguerrero@users.noreply.github.com>
2023-04-16 15:57:03 -0400

    Actualizar archivo query_empresa.js
    Se eliminó la función de Empresa (la vieja), ya que esa función se actualizó

--- app/empresa/query_empresa.js
+++ app/empresa/query_empresa.js
@@ -1,21 +1,7 @@
-// Importamos el until, la conexion con nuestra base de datos
-const util = require('util')
+// Importamos la conexion con nuestra base de datos
 const connection = require('../../config/connection')
-// Creamos una variable para realizar las consultas
-const query = util.promisify(connection.query).bind(connection)
 
 // Función para los errores de las peticiones a la base de datos
-
-async function Empresa(sql) {
-    try {
-        let sql_query = sql
-        const res = await query(sql_query)
-        return res
-    }catch(err) {
-        console.log(`Hubo un error : ${err}`)
-        return err
-    }
-
 async function Empresa(sql, values) {
     return new Promise((resolve, reject) => {
         connection.query(sql, values, (error, results) => {
@@ -27,7 +13,6 @@ async function Empresa(sql, values) {
             }
         })
     })
-
 }
 
 // Exportamos
