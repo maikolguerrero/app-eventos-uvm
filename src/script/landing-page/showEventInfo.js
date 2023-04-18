a5b0d7cf21bfdbb3b6c8e0bbcfeae4317654ece5
DAW-DUCKASWELL <114958225+DAW-DUCKASWELL@users.noreply.github.com>
2023-04-18 14:29:56 -0400

    Funcionalidad a el apartado de eventos.
    Menú desplegable añadido.

--- src/script/landing-page/showEventInfo.js
+++ src/script/landing-page/showEventInfo.js
@@ -1,29 +1,22 @@
 const btnSeeMore = document.querySelector('.events__see-more') //obtenemos elementos
 const moreInfo = document.querySelector('.events__more-info')
 const btnSeeLess = document.querySelector('.events__see-less')
-const eventContainer = document.querySelector('.events__event')
 const eventsContainer = document.querySelector('.events__list')
-const addClass = () =>{
-        moreInfo.classList.add('active')
-        btnSeeMore.classList.add('active')
-        btnSeeLess.classList.add('active')
-        eventContainer.classList.add('active')
-}
-
-eventsContainer.addEventListener('click', (e) =>{
-    if(e.target && e.target.classList.contains('events__see-more')){
-        addClass()
-        console.log('este es')
-    }
-});
-
-eventsContainer.addEventListener('click', (e) =>{
-    if(e.target && document.querySelector('.events__see-less')){
-        moreInfo.classList.remove('active')
-        btnSeeMore.classList.remove('active')
-        eventContainer.classList.remove('active')
-        btnSeeLess.classList.remove('active')
-    }
+eventsContainer.addEventListener('click', (evento) =>{
+    if(evento.target && evento.target.matches('.events__see-more')){
+        let eC = evento.target.parentNode.parentNode
+        let eC2 = eC.querySelectorAll('.events__see-more, .events__more-info, .events__see-less')
+        eC2.forEach(element => {
+              element.classList.add('active')
+          });
+        }else if(evento.target && evento.target.matches('.events__see-less')){
+            let eC = evento.target.parentNode.parentNode
+            let eC2 = eC.querySelectorAll('.events__see-more, .events__more-info, .events__see-less')
+            eC2.forEach(element => {
+              element.classList.remove('active')
+          });
+        }
+        
 })
 // btnSeeMore.addEventListener('click', ()=>{
 //         moreInfo.classList.add('active')
