5852b790d88d04ea05bc9cc4d40a1bdfc01fa3bf
DAW-DUCKASWELL <114958225+DAW-DUCKASWELL@users.noreply.github.com>
2023-04-18 14:37:27 -0400

    Código js comentado
    Código js de eventos comentado.

--- src/script/landing-page/showEventInfo.js
+++ src/script/landing-page/showEventInfo.js
@@ -2,14 +2,17 @@ const btnSeeMore = document.querySelector('.events__see-more') //obtenemos eleme
 const moreInfo = document.querySelector('.events__more-info')
 const btnSeeLess = document.querySelector('.events__see-less')
 const eventsContainer = document.querySelector('.events__list')
+
+//Damos un listener al contenedor padre para delegar los eventos
+
 eventsContainer.addEventListener('click', (evento) =>{
-    if(evento.target && evento.target.matches('.events__see-more')){
-        let eC = evento.target.parentNode.parentNode
-        let eC2 = eC.querySelectorAll('.events__see-more, .events__more-info, .events__see-less')
+    if(evento.target && evento.target.matches('.events__see-more')){//condicional para saber si si le está dando click al botón y si exsite un target
+        let eC = evento.target.parentNode.parentNode //capturamos el contendor padre en este caso .events__event
+        let eC2 = eC.querySelectorAll('.events__see-more, .events__more-info, .events__see-less') //seleccionamos los elementos quie necesitamos dentro de cada evento.
         eC2.forEach(element => {
-              element.classList.add('active')
+              element.classList.add('active') // a cada elemento le ponemos la clase active
           });
-        }else if(evento.target && evento.target.matches('.events__see-less')){
+        }else if(evento.target && evento.target.matches('.events__see-less')){ //lo mismo pero removiendo active
             let eC = evento.target.parentNode.parentNode
             let eC2 = eC.querySelectorAll('.events__see-more, .events__more-info, .events__see-less')
             eC2.forEach(element => {
@@ -18,17 +21,3 @@ eventsContainer.addEventListener('click', (evento) =>{
         }
         
 })
-// btnSeeMore.addEventListener('click', ()=>{
-//         moreInfo.classList.add('active')
-//         btnSeeMore.classList.add('active')
-//         btnSeeLess.classList.add('active')
-//         eventContainer.classList.add('active')
-// })
-
-// btnSeeLess.addEventListener('click', () =>{
-//     moreInfo.classList.remove('active')
-//     btnSeeMore.classList.remove('active')
-//     eventContainer.classList.remove('active')
-//     btnSeeLess.classList.remove('active')
-// })
-
