5a0d29bc7a72580b0c2b99e2455b1c766c15fa66
DAW-DUCKASWELL <114958225+DAW-DUCKASWELL@users.noreply.github.com>
2023-04-19 16:17:19 -0400

    Navbar cuando hay una sesión.
    Diseño de navbar cuando existe una sesión. Añadido resultados en la barra de busqueda.

--- src/script/navbar/navbar-mobile.js
+++ src/script/navbar/navbar-mobile.js
@@ -14,3 +14,29 @@ sBtn.addEventListener('click', () =>{
     searchBar.classList.toggle('active')
     bgBlur.classList.toggle('active')
 })
+
+//mostrar resultados de la barra de búsqueda
+const input = document.querySelector('.header__search-input')
+const results = document.querySelector('.header__search-results')
+
+window.addEventListener('click', (e) =>{
+    if(e.target && input.contains(e.target)){
+        results.classList.add('active')
+    }else{
+        results.classList.remove('active')
+    }
+})
+
+//Mostrar menú user
+const btnUser = document.querySelector('.header__user-img-btn')
+const userMenu = document.querySelector('.header__user-menu')
+
+btnUser.addEventListener('click',() =>{
+    userMenu.classList.toggle('active')
+})
+
+const btnUserDesk = document.querySelector('.header__user-img-desk-btn')
+
+btnUserDesk.addEventListener('click',() =>{
+    userMenu.classList.toggle('active')
+})
